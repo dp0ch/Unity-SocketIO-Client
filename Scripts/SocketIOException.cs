f26092b94bcc8207ff2973eb099e234874a0772d
dp0ch <dp0chdev@gmail.com>
2017-06-16 04:57:03 +0200

    init

---
+++ Scripts/SocketIOException.cs
@@ -0,0 +1,9 @@
+﻿namespace Dpoch.SocketIO {
+    using System;
+
+    public class SocketIOException : Exception {
+        public SocketIOException() : base() { }
+        public SocketIOException(string message) : base(message) { }
+        public SocketIOException(string message, Exception innerException) : base(message, innerException) { } 
+    }
+}
